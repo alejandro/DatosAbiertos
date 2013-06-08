0ace0296458871d8e7a2d69efbe6292da27b9a41
Byron Sommardahl <byron@sommardahl.com>
2013-06-08 07:14:12 -0700

    Removed trailing slash from appFog baseUrl on server.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 //only one shared url right now
 var url = 'http://localhost:3000';
-//var url = 'http://datosabiertoshn.aws.af.cm/';
+//var url = 'http://datosabiertoshn.aws.af.cm';
 
 module.exports = {
 	baseUrl : url
