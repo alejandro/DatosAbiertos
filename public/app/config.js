4925772f9a7d6c76c4f2e2468d208930702dc243
Byron Sommardahl <byron@sommardahl.com>
2013-06-08 06:44:18 -0700

    Added websiteBaseUrl for google.

--- public/app/config.js
+++ public/app/config.js
@@ -2,10 +2,13 @@ define(function(){
 		
 	var config = function(){
 		
+		//only one shared url right now
+		var url = 'http://localhost:3000/';
+		//var url = 'http://datosabiertoshn.aws.af.cm/';
 		
 		return {
-			apiBaseUrl : 'http://localhost:3000/',
-			//apiBaseUrl : 'http://datosabiertoshn.aws.af.cm/',
+			apiBaseUrl : url,
+			baswebsiteBaseUrleUrl: url 
 			loginPage: "/#/login"
 		};
 	}();
