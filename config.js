1115863f52f4c6c0c384488035c4eb6930db7e5f
Gazy Mahomar <gmahomarf@gmail.com>
2013-10-19 10:28:47 -0600

    Edited config.js to make baseUrl environment dependent

--- config.js
+++ config.js
@@ -1,10 +1,8 @@
-var url = 'http://localhost:3000';
-//var url = 'http://datosabiertoshn.aws.af.cm';
 
 module.exports = {
-	baseUrl : url,
   development: {
     port: process.env.PORT || 3000,
+    baseUrl: 'http://localhost:3000',
     db: {
       host: 'localhost',
       port: 27017,
@@ -13,6 +11,7 @@ module.exports = {
   },
   production: {
     port: process.env.PORT || 80,
+    baseUrl: 'http://datosabiertoshn.aws.af.cm',
     db: {
       host: 'localhost',
       port: 27017,
